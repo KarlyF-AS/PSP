fc917349fa74264effb046e350e56ab98dcbbf88
iKarly <kalbarransanchez@danielcastelao.org>
2025-09-25 22:31:49 +0200

    Update interfaz.java
    cambios en la clase interfaz

--- src/interfaz.java
+++ src/interfaz.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class interfaz {
+public class Interfaz {
     public static void main(String[] args) {
-        String host = " ";
         Scanner sc = new Scanner(System.in);
+        String host = "";
 
-        while (!host.equalsIgnoreCase("salir")){
-            System.out.printf("Intruduce el host o IP (o 'salir' para terminar): ");
+        while (!host.equalsIgnoreCase("salir")) {
+            System.out.print("Escribe un host o IP (o 'salir' para terminar): ");
             host = sc.nextLine();
 
-            if (!host.equalsIgnoreCase("salir")){
-                lanzador.ejecutarPing(host);
+            if (!host.equalsIgnoreCase("salir")) {
+                Lanzador.ejecutarPing(host);
             }
         }
+
+        System.out.println("Fin del programa.");
         sc.close();
-        System.out.println("Programa terminado.");
     }
 }
