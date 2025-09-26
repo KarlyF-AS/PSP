5c97845b9560ba75881d45a0213fdd2e7dc81810
iKarly <kalbarransanchez@danielcastelao.org>
2025-09-26 10:49:36 +0200

    Update interfaz.java
    cambios

--- src/interfaz.java
+++ src/interfaz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Interfaz {
+public class interfaz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String host = "";
@@ -10,7 +10,7 @@ public class Interfaz {
             host = sc.nextLine();
 
             if (!host.equalsIgnoreCase("salir")) {
-                Lanzador.ejecutarPing(host);
+                lanzador.ejecutarPing(host);
             }
         }
 
